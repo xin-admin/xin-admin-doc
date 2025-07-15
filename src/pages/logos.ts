561c94a599cb6ad59b71687bb50dc730157b5aa0
liu <2302563948@qq.com>
2025-07-15 09:07:23 +0800

    update: logo list

--- src/pages/logos.ts
+++ src/pages/logos.ts
@@ -1,5 +1,17 @@
 export default [
   {
+    logo: '/static/logos/php.png',
+    link: 'https://www.php.net'
+  },
+  {
+    logo: '/static/logos/js.png',
+    link: 'https://developer.mozilla.org/docs/Web/JavaScript'
+  },
+  {
+    logo: '/static/logos/typescript.jpg',
+    link: 'https://www.typescriptlang.org/'
+  },
+  {
     logo: '/static/logos/react.svg',
     link: 'https://react.dev/'
   },
@@ -22,5 +34,41 @@ export default [
   {
     logo: '/static/logos/react-route.webp',
     link: 'https://reactrouter.com/'
-  }
+  },
+  {
+    logo: '/static/logos/docusaurus.svg',
+    link: 'https://docusaurus.io/'
+  },
+  {
+    logo: '/static/logos/frankenphp.svg',
+    link: 'https://frankenphp.dev/'
+  },
+  {
+    logo: '/static/logos/deepseek.png',
+    link: 'https://www.deepseek.com/'
+  },
+  {
+    logo: '/static/logos/zustand.jpg',
+    link: 'https://zustand-demo.pmnd.rs/'
+  },
+  {
+    logo: '/static/logos/echarts.png',
+    link: 'https://echarts.apache.org/'
+  },
+  {
+    logo: '/static/logos/axios.png',
+    link: 'https://axios-http.com/'
+  },
+  {
+    logo: '/static/logos/mswjs.svg',
+    link: 'https://mswjs.io/'
+  },
+  {
+    logo: '/static/logos/tailwindcss.png',
+    link: 'https://tailwindcss.com/'
+  },
+  {
+    logo: '/static/logos/vite.svg',
+    link: 'https://vitejs.dev/'
+  },
 ]
